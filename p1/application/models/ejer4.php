753af640e6da78cd2a4f92157488a1aae1016b3b
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2018-04-17 19:51:16 -0300

    casi

--- p1/application/models/ejer4.php
+++ p1/application/models/ejer4.php
@@ -11,7 +11,21 @@ class Ejer4 extends CI_Model {
 	}
 	public function insertar($data){
 		if(isset($data['nombretutor'])){
-			#$resultado = $this->db->query("INSERT INTO 'ejer4'('tipo', 'nombre', 'dni', 'edad', 'nombretutor', 'dnitutor') VALUES ($data['tipo'],$data['nombre'],$data['dni'],$data['edad'],$data['nombretutor'],$data['dnitutor']");
+			$enviar = array(
+				'nombre' => $data['nombre'],
+				'edad' => $data['edad'],
+				'dni' => $data['dni'],
+				'nombretutor' => $data['nombretutor'],
+				'dnitutor' => $data['dnitutor'],
+				 );
+
+			$this->db->insert('ejer4',$enviar);
+
+			#$resultado = $this->db->query("INSERT INTO ejer4(tipo, nombre, dni, edad, nombretutor, dnitutor) VALUES ('juvenil',$nombre,$dni,$edad,$nombretutor,dnitutor");
+
+			/*INSERT INTO `ejer4`(`id`, `tipo`, `nombre`, `dni`, `edad`, `nombretutor`, `dnitutor`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])*/
+			/*("INSERT INTO 'ejer4'('tipo', 'nombre', 'dni', 'edad', 'nombretutor', 'dnitutor') VALUES ($data['tipo'],$data['nombre'],$data['dni'],$data['edad'],$data['nombretutor'],$data['dnitutor']");*/
+			#INSERT INTO 'ejer4' ('tipo', 'nombre', 'dni', 'edad', 'nombretutor', 'dnitutor') VALUES ('juvenil','$nombre','$dni','$edad','$nombretutor','$dnitutor'
 		}
 	}
 }
--- p1/application/views/validar.php
+++ p1/application/views/validar.php
@@ -9,10 +9,8 @@
 		if ($tipo == 'Juvenil' || $tipo == 'Infante'){
 			?>
 			<form action="<?php echo base_url();?>validar/registrar" method="post">
-				<select name = "tipo" disabled="on">
-				    <option value="<?php echo $tipo;?>"><?php echo $tipo;?></option>
-				 	
-				</select><br>
+				<input type="text" name="tipo" value="<?php echo $tipo;?>" disabled><br>
+				
 				<input type="text" name="nombre" value="<?php echo $nombre;?>" placeholder="Nombre"><br>
 				<input type="number" name="edad" value="<?php echo $edad;?>" placeholder="Edad"><br>
 				<input type="text" name="dni" value="<?php echo $dni;?>" placeholder="DNI"><br>
